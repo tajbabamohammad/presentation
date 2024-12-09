8f35d9be90f0212bad8bc8a0adcd9dcce4f85374
tajbaba <tajbabamohammad7@gmail.com>
2024-12-09 15:25:30 +0530

    Presentation.java

--- presentation.java
+++ presentation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class presentation {
+public class Presentation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a year: ");
